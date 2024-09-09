902745f16d96d89380c336c7a8f775e87b586f3c
Łukasz Prokulski <prokulski@gmail.com>
2024-09-09 11:10:35 +0200

    dzien 1

--- dzien_01/main.py
+++ dzien_01/main.py
@@ -1,1 +1,30 @@
+# wyświetlenie napisu w pythonie
 print("hello world!")
+
+
+# wyświetlenie napisu w Javie
+# class HelloWorld {
+#     public static void main(String[] args) {
+#         System.out.println("Hello, World!");
+#     }
+# }
+
+
+# PEP8 - długość linii
+# def funkcja(
+#     pierwszy,
+#     drugi,
+#     trzeci,
+#     czwarty,
+#     piaty,
+#     pierwszya,
+#     drugai,
+#     trzaeci,
+#     czawarty,
+#     paiaty,
+# ):
+#     pass
+
+
+# PEP8 - https://peps.python.org/pep-0008
+# po polsku - https://analityk.edu.pl/jak-pisac-kod-w-python-aby-byl-czytelny-pep8/
