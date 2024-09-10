cd7cd6d88099ccfdb156b416802de2b0428de20c
Łukasz Prokulski <prokulski@gmail.com>
2024-09-10 09:26:30 +0200

    dzien 2

--- dzien_02/main.py
+++ dzien_02/main.py
@@ -30,3 +30,41 @@
 # # pythonic-style:
 # for el in lista_a:
 #     print(el)
+
+
+# dodawanie elementów do listy
+lista = []
+print(lista)
+
+
+# dodajemy element "1" do listy
+lista.append(1)
+print(lista)
+
+lista.append(2)
+lista.append(2)
+lista.append(5)
+lista.append(2)
+
+lista.append("ala ma kota")
+print(lista)
+
+# usuwanie 
+lista.remove(2)
+print(lista)
+
+
+if 5 in lista:
+    print("5 jest na liście, usuwam")
+    lista.remove(5)
+print(lista)
+
+# wstawienie obiektu przed element o podanym indeksie - tutaj index=2
+lista.insert(2, "napis")
+print(lista)
+
+
+### ZADANIE
+
+# Napisz kod który umieści w liście 10 kolejnych potęg liczby 2.
+# Następnie przeiteruj po tej liście i każdy z jej elementów wyświetl na konsoli w osobnej linii.
