eb9b611980a3042e3c69770408f9945559c49cfe
Łukasz Prokulski <prokulski@gmail.com>
2024-09-10 09:12:40 +0200

    dzien 2

---
+++ dzien_02/main.py
@@ -0,0 +1,18 @@
+# listy
+# lista = list()
+# print(lista)
+
+# lista_b = []
+# print(lista_b)
+
+# lista = [1, 2, 3]
+# print(lista)
+
+# lista_a = [1, 2, 3, "ala", "e", 3.14, 1.23]
+# print(lista_a)
+
+
+# iteracja po liście
+lista_a = [1, 2, 3, "ala", "e", 3.14, 1.23]
+print(lista_a[3])
+
