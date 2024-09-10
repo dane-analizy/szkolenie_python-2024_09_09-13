d456965ebf182b5c8034203da5eb3db1f00c0736
Łukasz Prokulski <prokulski@gmail.com>
2024-09-10 09:19:19 +0200

    dzien 2

--- dzien_02/main.py
+++ dzien_02/main.py
@@ -12,7 +12,21 @@
 # print(lista_a)
 
 
-# iteracja po liście
-lista_a = [1, 2, 3, "ala", "e", 3.14, 1.23]
-print(lista_a[3])
+# # iteracja po liście
+# lista_a = [1, 2, 3, "ala", "e", 3.14, 1.23]
+# print(lista_a[3])
 
+# print(f"Lista 'lista_a' ma długość {len(lista_a)} elementów")
+
+
+# for n, element in enumerate(lista_a):
+#     print(f"Element o indeksie {n} : {element}")
+    
+    
+# # sposób bardzo łopatologiczny iteracji po liście - częsty "bład" początkującego:
+# for i in range(len(lista_a)):
+#     print(lista_a[i])
+    
+# # pythonic-style:
+# for el in lista_a:
+#     print(el)
