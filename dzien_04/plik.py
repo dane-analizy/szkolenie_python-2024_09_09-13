3e6c40fcc8952b362001d43d96533cbaca494c77
Łukasz Prokulski <prokulski@gmail.com>
2024-09-12 11:20:45 +0200

    dzien 4

--- dzien_04/plik.py
+++ dzien_04/plik.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 def wczytaj_linie(nazwa_pliku, enc="utf-8"):
     return [
         linia.strip() for linia in open(nazwa_pliku, "r", encoding=enc) if linia.strip()
@@ -9,9 +12,14 @@ def podziel_linie(linie_z_pliku, sep=";"):
 
 
 def wczytaj(nazwa_pliku, enc="utf-8", sep=";"):
+    # sprawdź czy plik istnieje
+    if not Path(nazwa_pliku).is_file():
+        raise FileNotFoundError
+
     linie_z_pliku = wczytaj_linie(nazwa_pliku, enc)
     lista_podzielona = podziel_linie(linie_z_pliku, sep)
     return lista_podzielona
 
 
 print(wczytaj("dane.csv"))
+
