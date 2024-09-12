583af5fcd4c8f80045d856c1dd0f75f2e30c6fd1
Łukasz Prokulski <prokulski@gmail.com>
2024-09-12 11:24:52 +0200

    dzien 4

--- dzien_04/plik.py
+++ dzien_04/plik.py
@@ -2,24 +2,39 @@ from pathlib import Path
 
 
 def wczytaj_linie(nazwa_pliku, enc="utf-8"):
+    """Wczytuje listę linii z pliku do listy stringów"""
     return [
         linia.strip() for linia in open(nazwa_pliku, "r", encoding=enc) if linia.strip()
     ]
 
 
 def podziel_linie(linie_z_pliku, sep=";"):
+    """Dzieli listę stringów na listę krotek, podział z 'sep'"""
     return [tuple(linia.split(sep)) for linia in linie_z_pliku]
 
 
 def wczytaj(nazwa_pliku, enc="utf-8", sep=";"):
+    """Funkcja wczytuje do listy krotek dane z pliku.
+
+    Args:
+        nazwa_pliku (str): ścieżka do pliku
+        enc (str, optional): encoding pliku, domyślnie "utf-8".
+        sep (str, optional): separator rozdzielający krotki w ramach linii, domyślnie ";".
+
+    Raises:
+        FileNotFoundError: jeśli nazwa_pliku nie jest plikiem lub nie istnieje
+
+    Returns:
+        _type_: lista krotek
+    """
     # sprawdź czy plik istnieje
     if not Path(nazwa_pliku).is_file():
         raise FileNotFoundError
 
     linie_z_pliku = wczytaj_linie(nazwa_pliku, enc)
     lista_podzielona = podziel_linie(linie_z_pliku, sep)
+
     return lista_podzielona
 
 
 print(wczytaj("dane.csv"))
-
