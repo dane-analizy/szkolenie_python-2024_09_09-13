58eb4e778a179371e7240b657b258badddf1bf9f
Łukasz Prokulski <prokulski@gmail.com>
2024-09-13 12:30:38 +0200

    dzien 5

--- dzien_05/app_flask/main.py
+++ dzien_05/app_flask/main.py
@@ -4,5 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def home():
+    return "<h1>Strona główna 1</h1>"
+
+
+@app.route("/kontakt")
+def kontakt():
+    return "Kontakt"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5000)
