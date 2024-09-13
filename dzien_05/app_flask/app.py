c8345885122f8104dde32a4f0e74760b6cb7466a
Łukasz Prokulski <prokulski@gmail.com>
2024-09-13 14:26:24 +0200

    dzien 5

--- dzien_05/app_flask/app.py
+++ dzien_05/app_flask/app.py
@@ -1,6 +1,6 @@
 # instalacja Flaska: pip install flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates_app")
 
@@ -27,8 +27,20 @@ def dodaj(a, b):
 ### ZADANIE
 
 # Bazując na endpoincie /dodaj przygotuj analogiczny z mnożeniem
+@app.route("/mnozenie/<a>/<b>")
+def mnozenie(a, b):
+    return {"a": a, "b": b, "suma": float(a) * float(b)}
 
 
+# opcjonalne argumenty
+@app.route("/argumenty")
+def argumenty():
+    return request.args
+
+
+@app.route("/parametry/<param>")
+def parametry(param):
+    return {"args": request.args, "param": param}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
